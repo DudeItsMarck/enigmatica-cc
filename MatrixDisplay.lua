44b2f63c62efe8c87852f93e251cd8af46402cb3
DudeItsMarck <marcklaurent@live.dk>
2022-02-04 04:24:12 +0100

    yas

--- MatrixDisplay.lua
+++ MatrixDisplay.lua
@@ -5,7 +5,7 @@ monitor.setCursorPos(1,1)
 local matrix = peripheral.wrap("back")
 matrix.open(1)
 
-local mn = matrix.getRemoteNames(1)
+local mn = "inductionMatrix_0"
 
 local percRes = matrix.callRemote(mn, "getEnergyFilledPercentage")
 local finalPerc = percRes * 100
