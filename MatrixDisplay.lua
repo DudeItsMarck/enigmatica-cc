45147193d40c42207c424e3eb68ce37d2895b5d2
DudeItsMarck <marcklaurent@live.dk>
2022-02-04 04:37:58 +0100

    .

--- MatrixDisplay.lua
+++ MatrixDisplay.lua
@@ -1,14 +1,18 @@
 local monitor = peripheral.wrap("left")
-monitor.clear()
-monitor.setCursorPos(1,1)
-
 local matrix = peripheral.wrap("back")
-matrix.open(1)
-
 local mn = "inductionMatrix_0"
+local percRes
+local finalPerc
 
-local percRes = matrix.callRemote(mn, "getEnergyFilledPercentage")
-local finalPerc = percRes * 100
-monitor.write("Induction Matrix filled: ")
-monitor.write(finalPerc)
-monitor.write("%")
+while true do
+    monitor.clear()
+    monitor.setCursorPos(1,1)
+
+    matrix.open(1)
+
+    local percRes = matrix.callRemote(mn, "getEnergyFilledPercentage")
+    local finalPerc = percRes * 100
+    monitor.write("Induction Matrix filled: ")
+    monitor.write(finalPerc)
+    monitor.write("%")
+end
